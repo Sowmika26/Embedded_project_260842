bf49c8809a5b00824384d1b2fc350fef9fb85949
Kurra Nagasowmika <64570694+Sowmika26@users.noreply.github.com>
2021-04-29 20:41:08 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -15,36 +15,37 @@
 
 void activity3(void){
 
-    SET_TIMER1A; //Set Timer1A which is fast PWM
-    PRESCALER_64; //prescalar 64
-    SET_PWM; //Set PWM as Output
+    SET_TIMER1A; // To Set Timer1A which is as fast as PWM.
+    PRESCALER_64; //prescalar as 64
+    SET_PWM; //To Set PWM as Output
 
 }
 
-void PWMoutput(uint16_t ADC_Value){
+char PWMoutput(uint16_t ADC_Value){
 
     if((ADC_Value>=0) && (ADC_Value<=200)){
 
-        OCR1A = 170;   //20% duty cycle
+        OCR1A = 204;   //20% duty cycle
         _delay_ms(20);
+        return 'a';
     }
     else if((ADC_Value>=210) && (ADC_Value<=500)){
 
-        OCR1A = 390;    //40% duty cycle
+        OCR1A = 409;    //40% duty cycle
         _delay_ms(20);
+       return 'b';
     }
     else if((ADC_Value>=510) && (ADC_Value<=700)){
 
-        OCR1A = 673;   //70% duty cycle
+        OCR1A = 716;   //70% duty cycle
         _delay_ms(20);
+        return 'c';
     }
     else if((ADC_Value>=710) && (ADC_Value<=1024)){
 
-        OCR1A = 973;   //95% duty cycle
+        OCR1A = 980;   //95% duty cycle
         _delay_ms(20);
-    }
-    else{
-        OCR1A = 0;  //0% duty cycle
+        return 'd';
     }
 
 }
