834161a1194bb8846f991cf5d0b4701205e6f3fa
KURRA NAGASOWMIKA <sowmikasandy62@gmail.com>
2021-04-30 10:12:08 +0530

    upgrade

--- src/activity3.c
+++ src/activity3.c
@@ -8,12 +8,16 @@
  * @copyright Copyright (c) 2021
  *
  */
-
 #include "activity3.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
-void activity3(void){
+
+/**
+ * @brief of activity3
+ *
+ */
+ void activity3(void){
 
     SET_TIMER1A; //Set Timer1A which is fast PWM
     PRESCALER_64; //prescalar 64
@@ -21,30 +25,83 @@ void activity3(void){
 
 }
 
-void PWMoutput(uint16_t ADC_Value){
+/**
+ * @brief
+ *
+ * @param ADC_Value
+ */
+ void PWMoutput(uint16_t ADC_Value){
+    int i;
+    char temperature[10];
 
     if((ADC_Value>=0) && (ADC_Value<=200)){
 
-        OCR1A = 170;   //20% duty cycle
+	OCR1A = 170; //20% duty cycle
+        temperature[0] = '2';
+        temperature[1] = '0';
+        temperature[2] = 176;
+        temperature[3] = 'C';
+        temperature[4] = ' ';
+        temperature[5] = '\0';
+        for(i=0;i<=5;i++){
+            UARTwritecharacter(temperature[i]);
+        }
         _delay_ms(20);
     }
     else if((ADC_Value>=210) && (ADC_Value<=500)){
 
-        OCR1A = 390;    //40% duty cycle
+        OCR1A = 390;    //40% of duty cycle
+	temperature[0] = '2';
+        temperature[1] = '5';
+        temperature[2] = 176;
+        temperature[3] = 'C';
+        temperature[4] = ' ';
+        temperature[5] = '\0';
+        for(i=0;i<5;i++){
+            UARTwritecharacter(temperature[i]);
+        }
         _delay_ms(20);
     }
     else if((ADC_Value>=510) && (ADC_Value<=700)){
 
-        OCR1A = 673;   //70% duty cycle
+        OCR1A = 670;   //80% of duty cycle
+	temperature[0] = '2';
+        temperature[1] = '9';
+        temperature[2] = 176;
+        temperature[3] = 'C';
+        temperature[4] = ' ';
+        temperature[5] = '\0';
+        for(i=0;i<=5;i++){
+            UARTwritecharacter(temperature[i]);
+        }
         _delay_ms(20);
     }
     else if((ADC_Value>=710) && (ADC_Value<=1024)){
 
-        OCR1A = 973;   //95% duty cycle
+        OCR1A = 970;   //100% of duty cycle
+	temperature[0] = '3';
+        temperature[1] = '3';
+        temperature[2] = 176;
+        temperature[3] = 'C';
+        temperature[4] = ' ';
+        temperature[5] = '\0';
+        for(i=0;i<=5;i++){
+            UARTwritecharacter(temperature[i]);
+        }
         _delay_ms(20);
     }
     else{
-        OCR1A = 0;  //0% duty cycle
+        OCR1A = 0;  //0% of duty cycle
+	temperature[0] = 'O';
+        temperature[1] = 'F';
+        temperature[2] = 'F';
+        temperature[3] = ' ';
+        temperature[4] = ' ';
+        temperature[5] = '\0';
+        for(i=0;i<=5;i++){
+            UARTwritecharacter(temperature[i]);
+        }
+	_delay_ms(20);
     }
 
 }
--- src/activity4.c
+++ src/activity4.c
@@ -13,62 +13,43 @@
 #include "activity1.h"
 #include <avr/interrupt.h>
 
+void UARTinit(uint16_t ubrr_value){
 
-void USARTInit(void){
+    UBRR0L = ubrr_value;
+    UBRR0H = (ubrr_value>>8)&(0x00ff);
+    UCSR0C = (1<<UMSEL00)|(1<<UCSZ00)|(1<<UCSZ01);
 
-    UBRR0L = BAUD_RATE;
-    UBRR0H = (BAUD_RATE>>8);
-    sei();
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
-    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
-    /**
-     * @brief Mode to select the Asynchronous and character size is 8-bit
-     *
-     */
-
-    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
-    /**
-     * @brief To Enable Transmitter and receiver
-     *
-     */
 }
 
 
-void USARTWriteString(char data){
-    if(data =='a'){
-        while(TRANSMITTING_DATA){};
-            Temperature = '2';
-            Temperature = '0';
-            Temperature = 176;
-            Temperature = 'C';
-            Temperature = ' ';
-            Temperature = '\0';
-    }
-    if(data =='b'){
-        while(TRANSMITTING_DATA){};
-            Temperature = '2';
-            Temperature = '5';
-            Temperature = 176;
-            Temperature = 'C';
-            Temperature = ' ';
-            Temperature = '\0';
-    }
-    if(data =='c'){
-        while(TRANSMITTING_DATA){};
-            Temperature = '2';
-            Temperature = '9';
-            Temperature = 176;
-            Temperature = 'C';
-            Temperature = ' ';
-            Temperature = '\0';
-    }
-    if(data =='d'){
-        while(TRANSMITTING_DATA){};
-            Temperature = '3';
-            Temperature = '3';
-            Temperature = 176;
-            Temperature = 'C';
-            Temperature = ' ';
-            Temperature = '\0';
-    }
+/**
+ * @brief A function to read characters coming from other UART port
+ *
+ * @return char
+ */
+char UARTreadchar(){
+
+while(UART_DATA_NOT_RECEIVED){
+
+}
+
+return UDR0;
+
+}
+
+
+/**
+ * @brief A function to write characters to send it to other UART port
+ *
+ * @param data
+ */
+void UARTwritecharacter(char data){
+
+while(UART_DATA_NOT_WRITTEN){
+
+}
+UDR0 = data;
+
 }
